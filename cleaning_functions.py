2516af0eb60e2e8d07b1d35ef2715492f8c6d1c7
jerome dharveng <jerome.dharveng@hotmail.com>
2020-11-18 17:53:09 +0100

    explore real estate scraped features

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -288,4 +288,235 @@ def encoding_df(cleaned_df, type="train", type_of_good='building'):
     return cleaned_df
 
 
+def remove_na(df, df_type='train', type_of_good='building', na_imputer_most_freq=None, na_imputer_median=None):
+    ''' Returns a cleaned DataFrame
+
+    :param df (pd.DataFrame): DataFrame to be cleaned
+    :param df_type (string): type of DataFrame to be cleaned : "train" or "test"
+    :param type_of_good (string): 'building' or 'apartment'
+    :param na_imputer_most_freq (Imputer): None if df_type == "train" else imputer fitted on the train set
+    :param na_imputer_median (Imputer): None if df_type == "train" else imputer fitted on the train set
+    :return (pd.DataFrame): returns a cleaned DataFrame (cleaned string notations, no missing values, adapted datatypes)
+    '''
+
+    # First Column Selection
+    if type_of_good == 'building':
+        selected_cols = ['immoweb_code', 'price', 'region', 'postcode', 'description', 'type_de_zone_inondable',
+                         'salles_de_bains', 'double_vitrage', 'chambres', 'revenu_cadastral', 'type_de_chauffage',
+                         'surface_du_terrain', 'surface_habitable', 'etat_du_batiment', 'annee_de_construction',
+                         'facades', 'classe_energetique']
+
+        # List of columns for imputation : "most_frequent"
+        list_col_names = ['chambres', 'salles_de_bains', 'etat_du_batiment', 'facades', 'annee_de_construction']
+
+        # List of columns where rows with missing values will be removed
+        not_na_cols = ['surface_du_terrain', 'surface_habitable', 'revenu_cadastral']
+
+        # List of columns to be converted to 'int'
+        int_cols = ['surface_du_terrain', 'surface_habitable', 'chambres', 'revenu_cadastral', 'salles_de_bains',
+                    'annee_de_construction', 'facades', 'price']
+
+
+    elif type_of_good == 'apartment':
+        selected_cols = ['immoweb_code', 'price', 'region', 'postcode', 'description', 'type_de_zone_inondable',
+                         'salles_de_bains', 'double_vitrage', 'chambres', 'revenu_cadastral', 'type_de_chauffage',
+                         'surface_habitable', 'etat_du_batiment', 'annee_de_construction', 'facades',
+                         'classe_energetique', 'terrasse', 'salles_de_douche', 'toilettes', 'parkings_exterieurs',
+                         'parkings_interieurs', 'cave', 'ascenseur', 'nombre_d_etages', 'etage']
+
+        # List of columns for imputation : "most_frequent"
+        list_col_names = ['chambres', 'etat_du_batiment', 'facades', 'etage', 'nombre_d_etages',
+                          'annee_de_construction']
+
+        # List of columns where rows with missing values will be removed
+        not_na_cols = ['revenu_cadastral', 'surface_habitable', 'salles_de_bains', 'toilettes']
+
+        # List of columns to be converted to 'int'
+        int_cols = ['surface_habitable', 'chambres', 'revenu_cadastral', 'salles_de_bains', 'salles_de_douche',
+                    'toilettes', 'etage', 'nombre_d_etages', 'parkings_exterieurs', 'parkings_interieurs',
+                    'annee_de_construction', 'facades', 'price']
+
+    else:
+        print('ERROR: type_of_good must be either "building" or "apartment')
+        return
+
+    # Checking that selected columns are in the given df
+    try:
+        df_new = df[selected_cols]
+    except:
+        # If not all preselected columns are in the given argument dataframe
+        # The missing columns will be added filled with np.nan
+        df_new = df.reindex(columns=selected_cols)
+
+    ### CLEANING AND GROUPING SOME STRING EXPRESSIONS
+    ## "type_de_zone_inondable" : 0 for non-flood zone and 1 otherwise
+    # First convert the NaN values to 'Zone non inondable'
+    df_new['type_de_zone_inondable'] = df_new['type_de_zone_inondable'].fillna('Zone non inondable')
+    no_flood = ['Zone non inondable',
+                'Bien immobilier situé tout ou en partie dans une zone riveraine délimitée']
+    df_new['type_de_zone_inondable'] = df_new['type_de_zone_inondable'].apply(lambda x: \
+                                                                                  0 if x in no_flood else 1)
+
+    ## Revenu Cadastrale
+    # clean first the non NaN values
+    df_new['revenu_cadastral'] = df_new['revenu_cadastral'].apply(lambda x: x.split("€")[-2] \
+        if isinstance(x, str) else x)
+
+    ## Area to live [m2]
+    df_new['surface_habitable'] = df_new['surface_habitable'].apply(lambda x: x.split('m²')[0] \
+        if isinstance(x, str) else x)
+
+    if type_of_good == 'building':
+        ## Area of the ground [m2]
+        df_new['surface_du_terrain'] = df_new['surface_du_terrain'].apply(lambda x: x.split('m²')[0] \
+            if isinstance(x, str) else x)
+
+    ## etat_du_batiment
+    # Group 'A rénover' and 'A restaurer' together as both are very similar terms
+    df_new['etat_du_batiment'] = df_new['etat_du_batiment'].apply(lambda x: 'À rénover' if x == 'À restaurer' \
+        else x)
+
+    ## classe_energetique
+    df_new['classe_energetique'] = df_new['classe_energetique'].apply(lambda x: clean_classe_en(x) if \
+        isinstance(x, str) else x)
+
+    ### Remove NA values
+    df_new = df_new.dropna(subset=not_na_cols)
+
+    ### FILLNA WTH CONSTANT VALUES
+    ## value = 'not_given'
+    not_given_cols = ['type_de_chauffage', 'classe_energetique', 'double_vitrage']
+    df_new[not_given_cols] = df_new[not_given_cols].fillna('not_given')
+
+    ## value = 0
+    if type_of_good == 'apartment':
+        zero_cols = ['terrasse', 'salles_de_douche', 'parkings_interieurs', 'parkings_exterieurs', 'cave', 'ascenseur']
+        df_new[zero_cols] = df_new[zero_cols].fillna(0)
+
+    ### IMPUTATION STRATEGIES
+    ## Apply 'most_frequent' imputation
+    df_new, imputer = imputing_missing_val(df_new, df_type, list_col_names, strategy='most_frequent', \
+                                           na_imputer=na_imputer_most_freq)
+
+    ## TYPE CONVERSION
+    df_new[int_cols] = df_new[int_cols].astype('int')
+
+    return df_new
+
+def encoding_4_exploration(cleaned_df, type="train", type_of_good='building'):
+    ''' Returns: an DataFrame where Categorical variables are encoded
+
+    :param cleaned_df (pd.DataFrame): DataFrame to be encoded
+    :param type (string): 'train' or 'test'
+    :param (string): 'building' or 'apartment'
+    :return (pd.DataFrame): a DataFrame where the Categorical Features have been encoded
+    '''
+    ## double_vitrage
+    cleaned_df['double_vitrage'] = cleaned_df['double_vitrage'].apply(lambda x: 1 if x == 'Oui' else 0)
+
+    ## Label Encode etat_du_batiment
+    good_state_classes = ['Excellent état', 'Fraîchement rénové', 'Bon', 'À rafraîchir', 'À rénover']
+    ordered_classes = pd.api.types.CategoricalDtype(ordered = True ,categories = good_state_classes)
+    cleaned_df['etat_du_batiment'] = cleaned_df['etat_du_batiment'].astype(ordered_classes)
+
+    ## Label Encode classe_energetique
+    energy_classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'not_given']
+    ordered_en_classes = pd.api.types.CategoricalDtype(ordered = True ,categories = energy_classes)
+    cleaned_df['classe_energetique'] = cleaned_df['classe_energetique'].astype(ordered_en_classes)
+
+    if type_of_good == 'apartment':
+        ## terrasse
+        cleaned_df['terrasse'] = cleaned_df['terrasse'].apply(lambda x: 1 if x == 'Oui' else 0).astype('int')
+        cleaned_df['cave'] = cleaned_df['cave'].apply(lambda x: 1 if x == 'Oui' else 0).astype('int')
+        cleaned_df['ascenseur'] = cleaned_df['ascenseur'].apply(lambda x: 1 if x == 'Oui' else 0).astype('int')
+
+    return cleaned_df
+
+################################### OUTLIERS ###################################
+
+
+def pct_method(df, col, level):
+    # Upper and lower limits by percentiles
+    upper = np.percentile(df[col], 100 - level)
+    lower = np.percentile(df[col], level)
+    # Returning the upper and lower limits
+    return [lower, upper]
+
+
+def iqr_method(df, col):
+    # Calculating the IQR
+    perc_75 = np.percentile(df[col], 75)
+    perc_25 = np.percentile(df[col], 25)
+    iqr_range = perc_75 - perc_25
+    # Obtaining the lower and upper bound
+    iqr_upper = perc_75 + 1.5 * iqr_range
+    iqr_lower = perc_25 - 1.5 * iqr_range
+    # Returning the upper and lower limits
+    return [iqr_lower, iqr_upper]
+
+def std_method(df, col):
+    # Creating three standard deviations away boundaries
+    std = np.std(df[col])
+    upper_3std = np.mean(df[col]) + 3 * std
+    lower_3std = np.mean(df[col]) - 3 * std
+    # Returning the upper and lower limits
+    return [lower_3std, upper_3std]
+
+
+def outlier_definition(df, col, level=1, continuous=False, can_be_neg=False):
+    # Obtaining the ranges
+    pct_range = pct_method(df, col, level)
+    iqr_range = iqr_method(df, col)
+    std_range = std_method(df, col)
+    if continuous is False:
+        # Setting the lower limit fixed for discrete variables
+        low_limit = np.min(df[col])
+        high_limit = np.max([pct_range[1],
+                             iqr_range[1],
+                             std_range[1]])
+    elif continuous is True:
+        if can_be_neg:
+            low_limit = np.min([pct_range[0],
+                                iqr_range[0],
+                                std_range[0]])
+        else:
+            low_limit = low_limit = np.max([pct_range[0],
+                                            iqr_range[0],
+                                            std_range[0]])
+
+        high_limit = np.max([pct_range[1],
+                             iqr_range[1],
+                             std_range[1]])
+
+    return [low_limit, high_limit]
+
+
+def limits_outliers(df, outlier_cols):
+    # Dictionary to keep the (low_limit, high_limit)
+    outliers_limits_dict = {}
+
+    for col in outlier_cols:
+        # Empirical test for continous feature or discrete one
+        if df[col].nunique() > 100:
+            outliers_limits_dict[col] = outlier_definition(df, col, level=1, continuous=True)
+        else:
+            outliers_limits_dict[col] = outlier_definition(df, col, level=1, continuous=False)
+
+    return outliers_limits_dict
+
+
+def remove_outliers(df, outlier_cols):
+    outliers_limits_dict = limits_outliers(df, outlier_cols)
+    df_no_outliers = df.copy()
+
+    for feature, limits in outliers_limits_dict.items():
+        # Make sure it's an int
+        df_no_outliers[feature] = df_no_outliers[feature].astype('int')
+        mask_low = df_no_outliers[feature] >= limits[0]
+        mask_high = df_no_outliers[feature] <= limits[1]
+        df_no_outliers = df_no_outliers[mask_low]
+        df_no_outliers = df_no_outliers[mask_high]
+
+    return outliers_limits_dict, df_no_outliers
+
 
--- exploration_functions.py
+++ exploration_functions.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
-import math
 import matplotlib.pyplot as plt
 import seaborn as sns
 from cleaning_functions import *
 
 
 ################################### PREPA UNIVARIATE PLOTS ###################################
+def encode_postcode(df):
+    ## Encoding the postcodes to longitude and latitude
+    df['long'] = df['postcode'].apply(lambda x: code2coord[int(x)][0])
+    df['lat'] = df['postcode'].apply(lambda x: code2coord[int(x)][1])
+    return df
 
 def prepa_energy_class(df):
     df_no_null = df.copy()
@@ -58,7 +62,7 @@ def prepa_surface(df, feature_name):
     return df_no_null
 
 
-def prepa_construction_yr(df):
+def prepa_construction_yr(df, agg_type='count'):
     df_no_null = df.copy()
 
     # Removing the null values
@@ -70,12 +74,22 @@ def prepa_construction_yr(df):
     # Help column for the groupby every 10 years
     df_no_null['decades'] = df_no_null['annee_de_construction'].apply(lambda x: (x // 10) * 10)
 
-    # Grouping by decades
-    df_grp_decades = df_no_null.groupby('decades').count()
+    if agg_type == 'count':
+        # Grouping by decades
+        df_grp_decades = df_no_null.groupby('decades').count()
+        prop_series = (df_grp_decades['annee_de_construction'] / df_no_null.shape[0])
+        return df_no_null, prop_series
 
-    prop_series = (df_grp_decades['annee_de_construction'] / df_no_null.shape[0])
+    elif agg_type == 'mean':
+        df_grp_decades = df_no_null.groupby('decades').mean()
+        return df_grp_decades
 
-    return df_no_null, prop_series
+    elif agg_type == 'boxplot':
+        return df_no_null
+
+    else:
+        print('agg_type should be either count, mean or boxplot')
+        return
 
 
 def prepa_4_not_null(df, feature_name):
@@ -134,6 +148,45 @@ def prepa_terrace_cellar_lift(df, features):
     return df_no_null
 
 
+def prepa_state(df):
+    df = df[df['etat_du_batiment'].notnull()]
+    state_classes = ['Excellent état', 'Fraîchement rénové', 'Bon', 'À rafraîchir', 'À rénover']
+    ordered_classes = pd.api.types.CategoricalDtype(ordered=True, categories=state_classes)
+    df['etat_du_batiment'] = df['etat_du_batiment'].astype(ordered_classes)
+
+    return df
+
+def from_postcode_2_region(postcode):
+    # source: https://en.wikipedia.org/wiki/List_of_postal_codes_in_Belgium
+    if postcode in range(1000,1300):
+        return 'bruxelles'
+    elif postcode in range(1300,1500):
+        return 'brabant-wallon'
+    elif postcode in range(1500,2000):
+        return 'brabant-flamand'
+    elif postcode in range(2000,3000):
+        return 'anvers'
+    elif postcode in range(3000,3500):
+        return 'brabant-flamand'
+    elif postcode in range(3500,4000):
+        return 'limbourg'
+    elif postcode in range(4000,5000):
+        return 'liege'
+    elif postcode in range(5000,6000):
+        return 'namur'
+    elif postcode in range(6000,6600):
+        return 'hainaut'
+    elif postcode in range(6600,7000):
+        return 'luxembourg'
+    elif postcode in range(7000,8000):
+        return 'hainaut'
+    elif postcode in range(8000,9000):
+        return 'flandre-occidentale'
+    elif postcode in range(9000,10000):
+        return 'flandre-orientale'
+    else:
+        return 'not_found'
+
 ################################### UNIVARIATE PLOTS ###################################
 
 def hist_1D(df, feature_name, type_of_good,xlabel,xlim, bins=None):
@@ -183,7 +236,7 @@ def proportion_barplot(df, feature_name, type_of_good, energy_class=False, order
     max_count = df[feature_name].value_counts().max()
     max_prop = (max_count / n_points)
 
-    # generate the tick mark locations and nammes
+    # generate the tick mark locations and names
     tick_props = np.arange(0, max_prop, ticks_scale)
     tick_names = ['{:0.2f}'.format(v) for v in tick_props]
 
@@ -197,6 +250,9 @@ def proportion_barplot(df, feature_name, type_of_good, energy_class=False, order
         if ordered:
             order = df[feature_name].value_counts().index
             ax = sns.countplot(data=df, x=feature_name, order=order, color="skyblue")
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=tilt)
+            plt.xlabel("")
+
         else:
             ax = sns.countplot(data=df, x=feature_name, color="skyblue")
             ax.set_xticklabels(ax.get_xticklabels(), rotation=tilt)
@@ -240,3 +296,60 @@ def draw_other_features(list_df, list_features, type_of_good, size=[20, 10], nro
         plt.subplot(nrows, ncols, i + 1)
         proportion_barplot(df_and_name[0], df_and_name[1], type_of_good, energy_class=False, ordered=False, \
                                create_fig=False)
+
+
+def violin_plot(df, feature_name_x, feature_name_y, type_of_good='building', size=[15, 6], ylabel='', tilt=30,
+                energy_class=False):
+    fig, ax = plt.subplots(figsize=size)
+
+    if energy_class:
+        # Defining color palette explicitely for energy_class
+        my_pal = {"A": "darkgreen", "B": "green", "C": "lightgreen", "D": "yellow", "E": "orange",
+                  "F": "darkorange", "G": "red"}
+        sns.violinplot(data=df, x=feature_name_x, y=feature_name_y, ax=ax, palette=my_pal)
+
+    else:
+        sns.violinplot(data=df, x=feature_name_x, y=feature_name_y, ax=ax, color='skyblue')
+
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=tilt)
+    ax.set_xlabel('')
+    ax.set_ylabel(ylabel, rotation=90, fontsize=12, labelpad=10)
+
+    ax.set(title=f'Violinplot of {feature_name_y} with regards to {feature_name_x} for the {type_of_good}s');
+
+
+def boxplot_2D(df, feature_name_x, feature_name_y, type_of_good='building', size=[15, 6], ylim=[0, 7000],
+               ylabel='', tilt=30, energy_class=False):
+    fig, ax = plt.subplots(figsize=size)
+
+    if energy_class:
+        # Defining color palette explicitely for energy_class
+        my_pal = {"A": "darkgreen", "B": "green", "C": "lightgreen", "D": "yellow", "E": "orange",
+                  "F": "darkorange", "G": "red"}
+
+        sns.boxplot(data=df, x=feature_name_x, y=feature_name_y, ax=ax, palette=my_pal)
+
+    else:
+        sns.boxplot(data=df, x=feature_name_x, y=feature_name_y, ax=ax, color='skyblue')
+
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=tilt)
+    ax.set_xlabel('')
+    ax.set_ylabel(ylabel, rotation=90, fontsize=12, labelpad=10)
+    ax.set_ylim(ylim)
+
+    ax.set(title=f'Boxplot of {feature_name_y} with regards to {feature_name_x} for the {type_of_good}s');
+
+
+def scatter_plot(df, feature_name_x, feature_name_y, hue_feature, type_of_good='building', size=[15, 6],
+                 xlim=[0, 6000], ylim=[0, 7000], xlabel='Revenu Cadastral [€]',
+                 ylabel='Price per square meter [€/m2]', alpha=0.4):
+    fig, ax = plt.subplots(figsize=size)
+
+    sns.scatterplot(data=df, x=feature_name_x, y=feature_name_y, hue=hue_feature, alpha=alpha)
+    ax.set_xlabel('')
+    ax.set_xlabel(xlabel, fontsize=12, labelpad=10)
+    ax.set_ylabel(ylabel, rotation=90, fontsize=12, labelpad=10)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    ax.set(title=f'Scatterplot of {feature_name_y} with regards to {feature_name_x} for the {type_of_good}s');
